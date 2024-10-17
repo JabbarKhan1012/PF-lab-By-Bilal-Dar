b235287ec9ec79180f71728ec8c91f59a535aa7f
JABBAR KHAN <baro78487@gmail.com>
2024-10-17 15:02:25 +0500

    lab4.task4

---
+++ fourth.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num_1;
+    int num_2;
+    cout<<"Enter the first number::";
+    cin>>num_1;
+    cout<<"Enter the second number::";
+    cin>>num_2;
+    if(num_1>num_2){
+        cout<<"The number one is greater than number two";
+    }
+    else
+    cout<<"the number one is less then number two";
+
+}
