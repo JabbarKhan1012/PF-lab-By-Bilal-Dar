4a40de870daa32550029af2ccf46a59af892858f
JABBAR KHAN <baro78487@gmail.com>
2024-10-17 15:01:50 +0500

    lab4.task2

---
+++ second.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num;
+cout<<"Enter the number::";
+cin>>num;
+if(num>0){
+    cout<<"The number is positive"<<endl;
+}
+else if(num=0){
+    cout<<"The number is 0";
+}
+else
+    cout<<"The number is negetive";
+}
